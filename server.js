2988a6c934e9e8657a89983a6b7924d7be3d8b16
minichambre <minichambre95@gmail.com>
2019-03-31 15:12:43 +0100

    initial

---
+++ server.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const schedule = require('node-schedule');
+
+var rule = new schedule.RecurrenceRule();
+rule.hour = 0;
+
+var j = schedule.scheduleJob(rule, () => {
+  console.log("Starting job");
+  getGuilds();
+});
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+});
+
+
+client.login('NTYxOTAwMTY0MjQwMTEzNjc1.XKC8xg.N1Bbw9GS6ZfMmYSAouJ2TwgfpxM');
+
+function getGuilds() {
+  //Returns the collection of all guilds(servers) that are using this bot.
+  //Loop through them one at a time
+  let guilds = client.guilds;
+  guilds.forEach( (guild) => {
+    if (guild.available){
+      searchChannels(guild);
+    }
+  })
+}
+
+//Search the channels of each guild for a christmas one
+function searchChannels(guild){
+  let date = new Date();
+  let channels = guild.channels;
+  channels.forEach( (channel) => {
+    if (channel.name == "christmas" || channel.name.includes("christmas")){
+      channel.setName(daysUntilChristmas()+" days until christmas") ;
+    }
+  });
+}
+
+//returns num of days until christmas
+function daysUntilChristmas(){
+  let date = new Date();
+  let christmas = new Date(date.getFullYear(), 11,25);
+
+  // If today is christmas, ITS CHRISTMAS
+  if (date.getMonth()==11 && date.getDate()==25)
+  {
+  return 0;
+  }
+  //If its between 25th-31st dec, we need next years christmas.
+  if (date.getMonth()==11 && date.getDate()>25)
+  {
+  christmas.setFullYear(cmas.getFullYear()+1);
+  }
+
+  let one_day=1000*60*60*24;
+  let days = Math.ceil((christmas.getTime()-date.getTime())/(one_day));
+  return days;
+}
